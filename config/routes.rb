b8bd3f37c7699904b24d3e60ec8848894fd61c7d
PatrickMartinII <139154280+PatrickMartinII@users.noreply.github.com>
2023-07-26 14:45:18 -0400

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  get 'submissions/create'
+  get 'submissions/show'
+  resources :quizzes do 
+    resources :questions
+  end   
+
+  devise_for :users
+
+  devise_scope :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  get 'home/index'
+  get 'home/course1'
+  get 'home/course2'
+  get 'home/course3'
+ 
+  root 'home#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
